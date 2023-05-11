2a491b9b14995bb9b1ee5bfa966964a90af42cad
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2023-05-10 20:27:16 -0700

    Add item multipler + materials missing

--- app/tabs/__init__.py
+++ app/tabs/__init__.py
@@ -30,8 +30,10 @@ class Manager(ttk.Frame):
         mainframe.scrollable_frame.grid_rowconfigure(1, minsize=10)
         recipe_frm.grid(column=0, row=2, columnspan=70, sticky=W)
         input_frm.grid(column=0, row=3, columnspan=70)
-        input_frm.grid_rowconfigure(1, minsize=10)
+        input_frm.grid_rowconfigure(1, minsize=20)
         mats_req_frm.grid(column=0, row=4, columnspan=70, sticky=W)
+        mainframe.scrollable_frame.grid_rowconfigure(5, minsize=10)
+        mats_missing_frm.grid(column=0, row=6, columnspan=70)
 
         # * Inventory
         # Label
@@ -55,11 +57,11 @@ class Manager(ttk.Frame):
         item_label.grid(column=0, row=0)
         # Entry
         self.item = StringVar()
-        item_entry = ttk.Entry(input_frm, textvariable=self.item, width=15)
-        item_entry.grid(column=1, row=0)
+        item_entry = ttk.Entry(input_frm, textvariable=self.item, width=20)
+        item_entry.grid(column=1, row=0, columnspan=2, sticky=W)
         # Save Button
         save_recipe = ttk.Button(input_frm, text='Save', command=self.save_recipe)
-        save_recipe.grid(column=2, row=0)
+        save_recipe.grid(column=3, row=0)
         item_entry.bind('<Return>', lambda e: save_recipe.invoke())
 
         # * Selecting the Item
@@ -69,10 +71,16 @@ class Manager(ttk.Frame):
         # Entry
         self.selected_item = StringVar()
         select_item_entry = ttk.Entry(input_frm, textvariable=self.selected_item, width=15)
-        select_item_entry.grid(column=1, row=2)
+        select_item_entry.grid(column=1, row=2, sticky=EW)
+        # Multiplier
+        self.multiplier = IntVar(value=1)
+        multiplier_entry = ttk.Entry(input_frm, textvariable=self.multiplier, width=3)
+        multiplier_entry.grid(column=2, row=2)
+        multiplier_entry.bind('<FocusIn>', lambda e: multiplier_entry.select_range(0, END))
+        multiplier_entry.bind('<Return>', lambda e: calculate.invoke())
         # Calculate Button
-        calculate = ttk.Button(input_frm, text='Calculate', command=self.calculate_mats)
-        calculate.grid(column=2, row=2)
+        calculate = ttk.Button(input_frm, text='Calculate', command=self.retrieve_mats)
+        calculate.grid(column=3, row=2)
         select_item_entry.bind('<Return>', lambda e: calculate.invoke())
 
         # * Materials Required
@@ -80,9 +88,18 @@ class Manager(ttk.Frame):
         mats_req_label = ttk.Label(mats_req_frm, text='Materials Required')
         mats_req_label.grid(column=0, row=0)
         # Textbox
-        self.masterlist = {}
-        self.mats_req = CustomText(mats_req_frm, width=70, height=10)
-        self.mats_req.grid(column=0, row=1, columnspan=70, rowspan=70)
+        self.mats_req = {}
+        self.mats_req_textbox = CustomText(mats_req_frm, state=DISABLED, width=70, height=10)
+        self.mats_req_textbox.grid(column=0, row=1, columnspan=70, rowspan=70)
+
+        # * Materials Missing
+        # Label
+        mats_missing_label = ttk.Label(mats_missing_frm, text='Materials Missing')
+        mats_missing_label.grid(column=0, row=0)
+        # Textbox
+        self.mats_missing = {}
+        self.mats_missing_textbox = CustomText(mats_missing_frm, state=DISABLED, width=70, height=10)
+        self.mats_missing_textbox.grid(column=0, row=1, columnspan=70, rowspan=10)
 
     def parse_inventory(self, *args):
         content = self.inventory.get('1.0', 'end').splitlines()
@@ -96,8 +113,6 @@ class Manager(ttk.Frame):
                 inventory[item] = quantity
             except:
                 pass
-
-        print(json.dumps(inventory, indent=4))
 
         file = open('data/inventory.json', 'w+')
         file.write(json.dumps(inventory, indent=4))
@@ -125,31 +140,47 @@ class Manager(ttk.Frame):
         self.item.set('')
         self.recipe.delete('1.0', 'end')
 
-    def find_ingredients(self, item):
+    def calc_mats_req(self, item):
         dir = os.listdir('data/recipes')
         target_file = '{}.json'.format(item.lower())
         for i, file in enumerate(dir): # go through the directory
             if file == target_file: # find the file that matches with one looking for
                 current_ingredient = target_file.split('.json')[0].title()
                 try:
-                    del self.masterlist[current_ingredient]
+                    del self.mats_req[current_ingredient]
                 except:
                     pass
                 selected_file = open('data/recipes/{}'.format(dir[i]), 'r') # open taht file up
                 recipe = json.loads(''.join(selected_file.readlines())) # load it into json
                 selected_file.close()
                 for ingredient in recipe:
-                    self.masterlist[ingredient] = recipe[ingredient] # for each ingredient in the recipe, add it to the masterlist
-                    self.find_ingredients(ingredient) # find the ingredients of each ingredient
+                    self.mats_req[ingredient] = recipe[ingredient] * self.multiplier.get() # for each ingredient in the recipe, add it to the masterlist
+                    self.calc_mats_req(ingredient) # find the ingredients of each ingredient
+    
+    def calc_mats_missing(self):
+        inventory_file = open('data/inventory.json', 'r')
+        inventory = json.loads(''.join(inventory_file.readlines()))
+        for ingredient in self.mats_req:
+            if ingredient in inventory:
+                qty_missing = self.mats_req[ingredient] - inventory[ingredient] 
+                if qty_missing > 0:
+                    self.mats_missing[ingredient] = qty_missing
+            else:
+                self.mats_missing[ingredient] = self.mats_req[ingredient]
 
-        # for ingredient in self.masterlist:
-        #     if target_file.split('.json')[0] == ingredient.lower():
-        #         del self.masterlist[ingredient]
-
-    def calculate_mats(self, *args):
-        self.find_ingredients(self.selected_item.get())
+    def retrieve_mats(self, *args):
+        self.calc_mats_req(self.selected_item.get())
         mats_req = []
-        for ingredient in self.masterlist:
-            mats_req.append('{} {}'.format(self.masterlist[ingredient], ingredient))
-        self.mats_req.insert('1.0', '\n'.join(mats_req))
-        print(json.dumps(self.masterlist, indent=4))
+        for ingredient in self.mats_req:
+            mats_req.append('{} {}'.format(self.mats_req[ingredient], ingredient))
+        self.mats_req_textbox['state'] = NORMAL
+        self.mats_req_textbox.insert('1.0', '\n'.join(mats_req))
+        self.mats_req_textbox['state'] = DISABLED
+
+        self.calc_mats_missing()
+        mats_missing = []
+        for ingredient in self.mats_missing:
+            mats_missing.append('{} {}'.format(self.mats_missing[ingredient], ingredient))
+        self.mats_missing_textbox['state'] = NORMAL
+        self.mats_missing_textbox.insert('1.0', '\n'.join(mats_missing))
+        self.mats_missing_textbox['state'] = DISABLED
